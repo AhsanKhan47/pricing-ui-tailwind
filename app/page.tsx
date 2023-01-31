6baba7c340b766e7b79261727ebd3169e201a9cd
Ahsankhan47 <ahsankhanafridi2000@gmail.com>
2023-02-01 03:21:16 +0500

    converterd to server comp

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,4 @@
-"use client";
-import Image from "next/image";
 import { Inter } from "@next/font/google";
-import styles from "../styles/page.module.css";
 import Header from "@/components/Header";
 import Cards from "@/components/Cards";
 
