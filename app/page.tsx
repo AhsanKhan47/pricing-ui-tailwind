6b136e9ed90fc2e92e4c8697c54d9cce93b95b41
Ahsankhan47 <ahsankhanafridi2000@gmail.com>
2023-02-01 03:15:20 +0500

    Capitalized component name for next build error

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,9 @@
+"use client";
 import Image from "next/image";
 import { Inter } from "@next/font/google";
 import styles from "../styles/page.module.css";
-import Header from "@/components/header";
-import Cards from "@/components/cards";
+import Header from "@/components/Header";
+import Cards from "@/components/Cards";
 
 const inter = Inter({ subsets: ["latin"] });
 
