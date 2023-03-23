c679e1d3dd42709325e03b443e17f4396f29669a
Ahsankhan47 <ahsankhanafridi2000@gmail.com>
2023-03-23 07:01:01 +0500

    button color updated

--- components/Cards.tsx
+++ components/Cards.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 import style from "../styles/page.module.css";
 export default function Cards() {
   return (
-    <section className="mt-[-80px] lg:flex lg:flex-row mlg:flex-wrap md:mt-[-130px]">
+    <section className="mt-[-80px] lg:flex  lg:flex-row lg:justify-around mlg:flex-wrap md:mt-[-130px]  lg:w-10/12 m-auto">
       <div className=" w-[280px] h-[304px] lg:w-[320px] lg:h-[404px]  bg-white mb-6 flex flex-wrap justify-center flex-col align-middle text-center m-auto shadow-2xl rounded-2xl">
         <img
           className={`${style.icon} m-auto`}
@@ -26,13 +26,13 @@ export default function Cards() {
           </li>
         </ul>
 
-        <button className="w-[50%] font-semibold  m-auto text-white text-[16px] lg:text-lg px-6 py-1 rounded-2xl bg-[#674FFF] hover:bg-[#ffffff] hover:text-[#674FFF] transition drop-shadow-lg ">
+        <button className="w-[50%] font-semibold  m-auto text-white text-[16px] lg:text-lg px-6 py-1 rounded-2xl bg-[#3A80F3] hover:bg-[#ffffff] hover:text-[#674FFF] transition drop-shadow-lg ">
           Join Now
         </button>
       </div>
 
       {/* Card 2 */}
-      <div className=" w-[280px] h-[304px] lg:w-[320px] lg:h-[404px]  bg-white mb-6 flex flex-wrap justify-center flex-col align-middle text-center m-auto shadow-2xl rounded-2xl">
+      <div className=" w-[280px] h-[304px] lg:w-[320px] lg:h-[404px]  lg:mx-6 bg-white mb-6 flex flex-wrap justify-center flex-col align-middle text-center m-auto shadow-2xl rounded-2xl">
         <img
           className={`${style.icon} m-auto`}
           src={`/premium.png`}
@@ -54,7 +54,7 @@ export default function Cards() {
           </li>
         </ul>
 
-        <button className="w-[50%] font-semibold  m-auto text-white text-[16px] lg:text-lg px-6 py-1 rounded-2xl bg-[#674FFF] hover:bg-[#ffffff] hover:text-[#674FFF] transition drop-shadow-lg ">
+        <button className="w-[50%] font-semibold  m-auto text-white text-[16px] lg:text-lg px-6 py-1 rounded-2xl bg-[#3A80F3] hover:bg-[#ffffff] hover:text-[#674FFF] transition drop-shadow-lg ">
           Join Now
         </button>
       </div>
@@ -85,7 +85,7 @@ export default function Cards() {
           </li>
         </ul>
 
-        <button className="w-[50%] font-semibold  m-auto text-white text-[16px] lg:text-lg px-6 py-1 rounded-2xl bg-[#674FFF] hover:bg-[#ffffff] hover:text-[#674FFF] transition drop-shadow-lg ">
+        <button className="w-[50%] font-semibold  m-auto text-white text-[16px] lg:text-lg px-6 py-1 rounded-2xl bg-[#3A80F3] hover:bg-[#ffffff] hover:text-[#674FFF] transition drop-shadow-lg ">
           Join Now
         </button>
       </div>
